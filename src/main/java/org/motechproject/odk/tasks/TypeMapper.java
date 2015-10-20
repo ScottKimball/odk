669e0dfa9bdfe3e5491a920be795664dc52447fd
Scott Kimball <scott.r.kimball@maine.edu>
2015-10-20 11:42:18 -0400

    fixed span tag in settings.html

--- src/main/java/org/motechproject/odk/tasks/TypeMapper.java
+++ src/main/java/org/motechproject/odk/tasks/TypeMapper.java
@@ -3,8 +3,6 @@ package org.motechproject.odk.tasks;
 import org.motechproject.odk.constant.FieldTypeConstants;
 import org.motechproject.odk.constant.TasksDataTypes;
 
-import java.util.HashMap;
-import java.util.Map;
 
 public class TypeMapper {
 
@@ -15,19 +13,19 @@ public class TypeMapper {
                 return  TasksDataTypes.INTEGER;
 
             case FieldTypeConstants.DATE_TIME:
-                return TasksDataTypes.UNICODE; // TODO
+                return TasksDataTypes.UNICODE;
 
             case FieldTypeConstants.BOOLEAN:
-                return TasksDataTypes.UNICODE; // TODO
+                return TasksDataTypes.UNICODE;
 
             case FieldTypeConstants.DECIMAL:
                 return TasksDataTypes.DOUBLE;
 
             case FieldTypeConstants.DATE:
-                return TasksDataTypes.UNICODE; // TODO
+                return TasksDataTypes.UNICODE;
 
             case FieldTypeConstants.TIME:
-                return TasksDataTypes.UNICODE; //TODO
+                return TasksDataTypes.UNICODE;
 
             case FieldTypeConstants.SELECT_1:
                 return TasksDataTypes.LIST;
